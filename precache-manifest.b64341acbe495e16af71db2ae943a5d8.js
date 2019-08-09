9dc426fef4e211a9885673a5f4d5bc2d1c7cd3a7
Vladislav <v-lipatov@yandex-team.ru>
2019-08-09 13:19:28 +0300

    Updates

---
+++ precache-manifest.b64341acbe495e16af71db2ae943a5d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e46f5c2c3de2e718026c4f39e301bd",
+    "url": "/as-test/index.html"
+  },
+  {
+    "revision": "c2ea6b65c7e54d5aac7f",
+    "url": "/as-test/static/css/main.20cfb930.chunk.css"
+  },
+  {
+    "revision": "ee50a2edb7f39d91f0d1",
+    "url": "/as-test/static/js/2.c7022880.chunk.js"
+  },
+  {
+    "revision": "c2ea6b65c7e54d5aac7f",
+    "url": "/as-test/static/js/main.34f62c50.chunk.js"
+  },
+  {
+    "revision": "534536ec585dffd13de8",
+    "url": "/as-test/static/js/runtime~main.34a1b28c.js"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/as-test/static/media/Check.68ca57dc.svg"
+  },
+  {
+    "revision": "0524587de253e236e54f36b597579be7",
+    "url": "/as-test/static/media/Logo.0524587d.svg"
+  }
+]);
